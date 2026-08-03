ce5640b0ffc9a57867a5b320bef103b1c86a0272
Sachin Gupta <161197824+SachinGupta369@users.noreply.github.com>
2026-08-03 15:38:44 +0530

    loopsChallenges.js

---
+++ Javascript/loopsChallenges.js
@@ -0,0 +1,22 @@
+/* 
+1. write a `while` loop that calculates the sum of all numbers from 1 to 5 and stores the result in a variable named `sum`.
+*/
+
+let sum = 0;
+let i = 1;
+
+while (i<=5) {
+    sum += i;
+    i++;
+}
+console.log(sum);
+
+/* 
+2. Write a `while` loop that counts down from 5 to 1 and stores the numbers in an array named `countdown`. 
+*/
+
+/* 
+3. write a `do while` loop that prompts a user to enter  their favorite tea type until they enter `"stop"`. 
+    Store each tea type in an array named `teaCollection`.
+*/
+
