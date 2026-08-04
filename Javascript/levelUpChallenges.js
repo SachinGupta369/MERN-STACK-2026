eca03520e653e4f339fff6e276ed45646fe2d482
Sachin Gupta <sachingupta0098@gmail.com>
2026-08-04 18:41:42 +0530

    levelUpChallenges.js

---
+++ Javascript/levelUpChallenges.js
@@ -0,0 +1,15 @@
+/* 
+1. Write a `for` loop that loops through the array `["Green tea", "black tea", "chai", "Oolong chai"]` and stop the loop when it find "chai".
+*/
+
+
+
+/* 
+2. Write a `for` loop that loops through the array `["Londo", "New York", "Paris", "Berlin"]` and skips `"Paris"`.
+
+Store the other cities in a new array named `"visitedCities"`
+*/
+
+/* 
+
+*/
