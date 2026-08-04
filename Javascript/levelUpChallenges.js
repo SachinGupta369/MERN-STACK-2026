cb10b88ca1878337a738dcd9792d5b3d0a265354
Sachin Gupta <sachingupta0098@gmail.com>
2026-08-04 20:44:33 +0530

    levelUpChallenges.js

--- Javascript/levelUpChallenges.js
+++ Javascript/levelUpChallenges.js
@@ -11,5 +11,63 @@ Store the other cities in a new array named `"visitedCities"`
 */
 
 /* 
+3. Usa a `for-of` loop to iterate through the array `[1,2,3,4,5]` and stop when the number `4` is found. 
+Store the numbers before `4` in an array named `smallNumbers`. 
+*/
 
+/* 
+4. Use a `for-of` loop to iterate through the array `["chai", "green tea", "herbal tea", "black tea"]` and skip `"herbal tea"`.
+
+Store the other teas in an array named `preferredTeas`.
+*/
+
+/* 
+5. Use a `for-in` loop to loop through an object containing city populations.
+
+Stop the loop when the population of `"Berlin"` is found and store all previous cities population in a new object named `cityPopulations`.
+
+let citiesPopulation = {
+"London":8900000,
+"New York":8400000,
+"Paris":2200000,
+"Berlin":3500000
+};
+
+*/
+
+/* 
+6. Use a `for-in` loop to loop through an object containing city populations.
+Skip any city with a population below 3 million and store the rest in an new object named `largeCities`.
+
+let worldCities = {
+    "Syndey":5000000,
+    "Tokyo": 9000000,
+    "Berlin":3500000,
+    "Paris":2200000
+};
+*/
+
+/* 
+7. Write a `forEach` loop that iterates through the array `["earl grey", "green tea", "chai", "oolong tea"]`.
+
+Stop the loop when `"Chai"` is found, and store all previous tea types in an array named `availableTeas`.
+*/
+
+/* 
+8. Write a `forEach` loop that iterates through the array `["Berlin", "Tokyo", "Sydney", "Paris"]`.
+
+Skip `"Syndey"` and store the other cities in a new array named `traveledCities`.
+*/
+
+/* 
+9. Write a `for` loop that iterate through the array `[2,5,7,9]`.
+
+Skip the value `7` and multiply the rest by 2. Store the result in a new array named `doubledNumbers`.
+*/
+
+/* 
+10. Use a `for-of` loop to iterate through the array `["chai", "Green tea", "black tea", "jasmine tea", "Herbal tea"]`. 
+
+And stop when the length of the current tea name is greater than 10.
+Store the teas iterated over in an array named `shortTeas`.
 */
