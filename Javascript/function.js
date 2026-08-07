d3c8861eec45d7ce9b8d18be3296e098c163b7d8
Sachin Gupta <sachingupta0098@gmail.com>
2026-08-07 20:31:09 +0530

    function.js

--- Javascript/function.js
+++ Javascript/function.js
@@ -10,3 +10,20 @@ message like `"Order confirmed for chai"`.
 Call `confirmOrder` from within `orderTea` and return the result. 
 */
 
+/* 
+3. Write an arrow function named `calculateTotal` that takes two parameters: `price` and `quantity`. The function should return the total cost by multiplying the `price` and `quantity`.
+
+Store the result in a variable named `totalCost`.
+*/
+
+/* 
+4. Write a function named `processTeaOrder` that takes another function, `makeTea`, as a parameter and calls it with the argument `"earl grey"`.
+
+Return the result of calling `makeTea`.
+*/
+
+/* 
+Write a function named `createTeaMaker` that returns another function. The returned function should take one parameter, `teaType`, and return a message like `"Making green tea"`
+
+Store the returned function in a variable named  `teaMaker` and call it with a `"green tea"`.
+*/
